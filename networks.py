32b16f353a053ab0310693d384ec86128737ec14
avasuilia <avalia44@gmail.com>
2021-03-07 21:49:11 +0530

    5th correction

--- networks.py
+++ networks.py
@@ -296,7 +296,7 @@ def Generator(name,img_size, img_ch,style_dim):
     inpA=Input(shape=(img_size,img_size,img_ch))
     inpB=Input(shape=(style_dim,))
     g0 = tf.keras.layers.ZeroPadding2D((0,1))(inpA)
-    g1 = conv2d(g0, 256, kernel_size=(h,3), strides=1, padding='valid')
+    g1 = conv2d(g0, 256, kernel_size=(img_size,3), strides=1, padding='valid')
     g2 = conv2d(g1, 256, kernel_size=(1,9), strides=(1,2))
     g3 = conv2d(g2, 256, kernel_size=(1,7), strides=(1,2))
     #upscaling
@@ -304,7 +304,7 @@ def Generator(name,img_size, img_ch,style_dim):
     g5 = AdaIN(data_format='channels_last')([g4,inpB])
     g6 = deconv2d(g5,g1, 256, kernel_size=(1,9), strides=(1,2), bnorm=False)
     g7 = AdaIN(data_format='channels_last')([g6,inpB])
-    g8 = ConvSN2DTranspose(1, kernel_size=(h,1), strides=(1,1), kernel_initializer=init, padding='valid', activation='tanh')(g7)
+    g8 = ConvSN2DTranspose(1, kernel_size=(img_size,1), strides=(1,1), kernel_initializer=init, padding='valid', activation='tanh')(g7)
     return Model([inpA,inpB],g8,name=name)
 
 class MappingNetwork(tf.keras.Model):
